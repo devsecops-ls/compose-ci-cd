2565edd485b6a8f26823ebc5787b9425c92f3846
devsecops-ls <alin.stefan.mociofleaca1@gmail.com>
2022-07-06 23:23:07 +0300

    fixed apis

--- tests.py
+++ tests.py
@@ -1,15 +1,17 @@
 import requests
 import time
+import os
 
 if __name__ == "__main__":
-    test_req1 = requests.get("http://localhost/news?country=gr")
+    IP_ADDRESS_API = os.getenv("IP_ADDRESS_API")
+    test_req1 = requests.get("http://{IP_ADDRESS_API}:3001/news?country=gr")
 
     if test_req1.status_code != 200:
         exit(1)
     else:
         print("News API checked")
 
-    test_req2 = requests.get("http://localhost/weather?city=amsterdam")
+    test_req2 = requests.get("http://{IP_ADDRESS_API}:3001/weather?city=amsterdam")
     if test_req2.status_code != 200:
         exit(1)
     else:
