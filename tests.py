d7c3d31ef75453ef9357fae657dd4a4ea7a5a175
devsecops-ls <105318015+devsecops-ls@users.noreply.github.com>
2022-07-31 22:25:39 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,21 +1,21 @@
 import requests
 import time
 import os
-
+ 
 if __name__ == "__main__":
-
-    # to get the IP: docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}'  compose-ci-cd_master_1
-    IP_ADDRESS_API = os.getenv("IP_ADDRESS_API")
-    print(f"Address API: {IP_ADDRESS_API}")
-    test_req1 = requests.get(f"http://{IP_ADDRESS_API}:3001/news?country=gr")
-
+      
+    HOSTNAME = os.getenv("HOSTNAME")
+    print(f"Address API: {HOSTNAME}")
+    test_req1 = requests.get(f"http://{HOSTNAME}:3001/news?country=gr")
+                      
     if test_req1.status_code != 200:
+        print("News API check failed")
         exit(1)
     else:
-        print("News API checked")
-
-    test_req2 = requests.get(f"http://{IP_ADDRESS_API}:3001/weather?city=amsterdam")
+        print("News API checked")                                                       
+    test_req2 = requests.get(f"http://{HOSTNAME}:3001/weather?city=amsterdam")
     if test_req2.status_code != 200:
+        print("Weather API check failed")
         exit(1)
     else:
         print("Weather API checked")
